55434ca76fc1643fc18405fc9e63b7946f98cfa3
Ibrahim-Alqarni <63380007+Ibrahim-Alqarni@users.noreply.github.com>
2021-04-18 21:02:42 -0400

    Add files via upload

---
+++ p1-ialqarni.py
@@ -0,0 +1,12 @@
+"""
+P1: Setting up your Python Environment
+Author: Ibrahim Alqarni
+Date: 02/03/2020 
+Python version 3.8.1
+"""
+
+fname: str= input("Pleas enter your first name:")
+lname: int= input("What is your last name, {0}? ".format(fname))
+print(f"Hello",fname,lname)
+
+
