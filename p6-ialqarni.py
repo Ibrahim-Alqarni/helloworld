ae4c16a7a4c6576519a2cd1eed3bbc069656b00f
Ibrahim-Alqarni <63380007+Ibrahim-Alqarni@users.noreply.github.com>
2021-04-25 14:56:43 -0400

    Add files via upload

---
+++ p6-ialqarni.py
@@ -0,0 +1,43 @@
+"""
+P6: Slicing & Dicing files
+Author: Ibrahim Alqarni
+Date: 03/09/2020 
+Python version 3.8.1
+version: 1.0
+"""
+
+import os
+
+def reading_file(file):
+    counter = 0
+    total = 0
+    filesize = os.path.getsize(file)
+
+    if filesize == 0:
+        print("The file is empty")
+
+    else:
+        try:
+            read_file= open(file,"r")
+        except FileNotFoundError:
+            print ("File name not found", file)
+        except NameError:
+            print("File name cannont oppend",file)
+
+        else:
+            try:
+                for line in read_file:
+                    line.strip("\n")
+                    if not line.startswith("X-DSPAM-Confidence:"):
+                        continue
+                    else:
+                        counter+=1
+                        num = float(line[21:])
+                        total = num + total
+                avrg = str(round(total/counter,4))
+                print("Average spam confidence: ", avrg)
+            except ValueError:
+                print("The file has invalid content")
+                
+file_name = input("Enter the file name: ")
+reading_file(file_name)
